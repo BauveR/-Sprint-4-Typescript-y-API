6c0969bb74e650824facdf6e1acc81e2f3777c97
BauveR <rick.bauve@gmail.com>
2025-06-11 23:35:46 +0200

    import the interface joke  then a function async fetch joke with a promise , using try catch to report the errors  an return data, also the html basic with ids

--- src/models/joke.model.ts
+++ src/models/joke.model.ts
@@ -1,1 +1,29 @@
+import { IJoke } from "../interface/Ijoke.interface.ts";
+
+export class JokeModel {
+
+  public static async fetchJoke(): Promise<IJoke> {
+    try {
+      const url = "https://icanhazdadjoke.com/"
+      const response = await fetch(url, {
+        headers: { "Accept": "application/json" }
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error Status: ${response.status}`);
+      }
+
+      const data: IJoke = await response.json();
+      return data;
+
+    } catch (error) {
+      console.error("Error fetching joke", error);
+      return {
+        id: "error",
+        joke: "the joke is not available",
+        status: 500
+      };
+    }
+  }
+}
 
