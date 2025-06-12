72d4395fec50cef3d1f38499cdf3c8be64376394
BauveR <rick.bauve@gmail.com>
2025-06-12 10:03:50 +0200

    Joke controller is an intermediary between joke model & the  joke view to show the user the joke

---
+++ src/controllers/joke.controller.ts
@@ -0,0 +1,29 @@
+import type { IJoke } from "../interface/Ijoke.interface";
+import { JokeModel } from "../models/joke.model";
+
+export class JokeController{
+    private model: typeof JokeModel;
+    private view: JokeView;
+
+    constructor(){
+        this.model = JokeModel;
+        this.view = new JokeView();
+        this.view.bindGetJoke(this.handleGetJoke.bind(this));
+    }
+
+    private async handleGetJoke():Promise<void>{
+        try{
+            this.view.showLoading();
+            const JokeData: IJoke = await this.model.fetchJoke();
+
+            if (JokeData.status && JokeData.status >= 400){
+                this.view.showError(JokeData.joke);
+            }else{
+                this.view.displayJoke(JokeData.joke);
+            }
+        }catch(error){
+            this.view.showError("Error al obtener el chiste");
+            console.error(error);
+        }
+    }
+}
